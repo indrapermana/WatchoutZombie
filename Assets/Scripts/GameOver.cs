a5dd1fe4e2ee173953ff1eab655da93523522d5e
Indra Permana <indrapermana.jtk10@gmail.com>
2017-12-23 00:07:22 +0800

    First commit WatchOutZombie

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    float timer = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 3f)
+        {
+            Data.score = 0;
+            SceneManager.LoadScene("Gameplay");
+        }
+    }
+}
---
+++ Assets/Scripts/MonsterMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MonsterMove : MonoBehaviour {
+
+    GameObject player;
+    Animator animator;
+    // Use this for initialization
+    void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(player.transform);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GameOver");
+        }
+        if (collision.gameObject.tag.Equals("Peluru"))
+        {
+            Data.score++;
+            Destroy(gameObject);
+        }
+    }
+}
